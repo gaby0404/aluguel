e2d613143829ca5b62f7f073a5322d793af629cc
GABRIELLY SANTOS DE OLIVEIRA <gabrielly.oliveira49@senaisp.edu.br>
2026-08-03 11:24:50 -0300

    Criação de tabelas (Models)

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Imovel(models.Model):
+    titulo = models.CharField(max_length=100)
+    tipo = models.CharField(max_length=100)
+    valor_aluguel = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.BooleanField(default=True)
+    logradouro = models.CharField(max_length=200)
+    cep = models.CharField(max_length=12)
+    complemento = models.CharField(max_length=100, blank=True, null=True)
+    bairro = models.CharField(max_length=100)
+    cidade = models.CharField(max_length=100)
+    uf = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.titulo
